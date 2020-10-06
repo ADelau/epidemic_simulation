1647a18af4631ae9d3a0e52871d61bd14c5f146b
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2020-10-06 16:48:13 +0200

    update testing to include number tested and account for the fact that hospitalized are always tested positive

--- cpp-simulator/outputs/process_output.py
+++ cpp-simulator/outputs/process_output.py
@@ -32,22 +32,77 @@ if __name__ == "__main__":
 	data = pd.merge(data, cases, on="Time")
 	data = data.iloc[::4, :]
 	data = data.reset_index(drop=True)
-	new_symptomatics = np.zeros((num_steps,))
+	data["num_cases"] = data["num_cases"].astype(int)
+	new_symptomatics = np.zeros((num_steps,)).astype(int)
 
-	for i in range(num_steps -1, 0, -1):
-		new_symptomatics[i] = data["num_cases"].iloc[i] - data["num_cases"].iloc[i-1]
+	#Store whether symptomatics tested as positive and already been hospitalized
+	symptomatics = np.zeros((data["num_cases"].iloc[-1], 2), dtype=np.bool_)
+	num_tested = np.zeros((num_steps,))
+	num_positive = np.zeros((num_steps,))
+	
+	next_cases = data["num_cases"].iloc[0]
+	new_hospitalized = data["num_cumulative_hospitalizations"].iloc[0]
+
 	new_symptomatics[0] = data["num_cases"].iloc[0]
+	for j in range(new_symptomatics[0]):
+		if np.random.random() < FRAC_TESTED:
+			num_tested[0] = num_tested[0] + 1
+			
+			if np.random.random() < FRAC_POSITIVE:
+				num_positive[0] = num_positive[0] + 1
+				symptomatics[j, 0] = True
 
-	new_symptomatics = new_symptomatics.astype(int)
+	for j in range(new_hospitalized):
+		while True:
+			index = np.random.randint(low=0, high=next_cases)
+			if not symptomatics[index, 1]:
+				symptomatics[index, 1] = True
+				if not symptomatics[index, 0]:
+					symptomatics[index, 0] = True
+					num_tested[i] = num_tested[i] + 1
+					num_positive[i] = num_positive[i] + 1
 
-	data["new_symptomatics"] = pd.Series(new_symptomatics)
-	print(new_symptomatics)
-	print(data["new_symptomatics"])
+				break
+
+	for i in range(1, num_steps):
+		curr_cases = data["num_cases"].iloc[i-1]
+		next_cases = data["num_cases"].iloc[i]
+		new_symptomatics[i] = data["num_cases"].iloc[i] - data["num_cases"].iloc[i-1]
+		new_hospitalized = data["num_cumulative_hospitalizations"].iloc[i] - data["num_cumulative_hospitalizations"].iloc[i-1]
+
+		for j in range(new_symptomatics[i]):
+			if np.random.random() < FRAC_TESTED:
+				num_tested[i] = num_tested[i] + 1
+				
+				if np.random.random() < FRAC_POSITIVE:
+					num_positive[i] = num_positive[i] + 1
+					symptomatics[curr_cases + j, 0] = True
+
+		for j in range(new_hospitalized):
+			while True:
+				index = np.random.randint(low=0, high=next_cases)
+				if not symptomatics[index, 1]:
+					symptomatics[index, 1] = True
+					if not symptomatics[index, 0]:
+						symptomatics[index, 0] = True
+						num_tested[i] = num_tested[i] + 1
+						num_positive[i] = num_positive[i] + 1
+
+					break
+
+
+
+	data["new_symptomatics"] = pd.Series(new_symptomatics).astype(int)
+	data["num_tested"] = pd.Series(num_tested).astype(int)
+	data["num_positive"] = pd.Series(num_positive).astype(int)
 
 	np.random.seed(SEED)
+
+	"""
 	num_tested = np.random.binomial(new_symptomatics, FRAC_TESTED)
 	num_positive = np.random.binomial(num_tested, FRAC_POSITIVE)
-	data["num_positive"] = num_positive
+	data["num_positive_old"] = num_positive
+	"""
 	data["Day"] = data["Time"].astype("int64")
 
 	data.drop("Time", inplace=True, axis=1)
@@ -64,13 +119,11 @@ if __name__ == "__main__":
 			index = i
 			break
 
-	print(data)
 	indexDay = data["Day"].iloc[index]
-	print(index)
 
 	data.drop(list(range(0, index)), inplace=True, axis=0)
 	data["Day"] = data["Day"] - indexDay + 1
 
 
-	data = data[["Day", "num_positive", "num_hospitalised", "num_cumulative_hospitalizations", "num_critical", "num_fatalities"]]
+	data = data[["Day", "num_positive", "num_tested", "num_hospitalised", "num_cumulative_hospitalizations", "num_critical", "num_fatalities"]]
 	data.to_csv(os.path.join(dir_name, "processed.csv"), index=False)
